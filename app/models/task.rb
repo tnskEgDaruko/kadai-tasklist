e686a2d03ee1f580aaae3e747625cbc67d971eb9
Daruko <tnskeg_daruko@yahoo.co.jp>
2018-07-26 22:12:42 +0000

    correct validates&Bootstrap

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, format: { with: /★+/ }, length: { maximum: 3 }
 end
