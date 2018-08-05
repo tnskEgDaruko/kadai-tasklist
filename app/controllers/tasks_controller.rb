0ff2061aa8fb484424196616b7a754ede5bb53d0
Daruko <tnskeg_daruko@yahoo.co.jp>
2018-08-05 12:35:16 +0000

    correct task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def show
@@ -43,10 +42,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
